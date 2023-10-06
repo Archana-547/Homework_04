0e4ff7ca2aae4c0e4aa9ef739e620b190ed1a2cd
Archana Balakrishna Lokhande <145834338+Archana-547@users.noreply.github.com>
2023-10-06 23:05:35 +0300

    Homework4 is completed added this file

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+public class Car {
+    String brand;
+    String model;
+    int year;
+
+    public void startEngine(){
+        System.out.println("car's engine has started");
+    }
+    public void stopEngine(){
+        System.out.println("car's engine has stopped");
+    }
+}
+
